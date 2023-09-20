c37f0d6eb6e9c0f9fe6b7ab722ab6a621a5fc05b
Prayas Dhand <prayasdhand13@gmail.com>
2023-09-20 16:32:25 +0530

    Worked Again on this Projject

---
+++ src/app/authorization.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from './services/api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthorizationGuard implements CanActivate {
+  constructor(private api: ApiService) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.api.isLoggedIn()) {
+      if (this.api.getTokenUserInfo()?.userType === "ADMIN") {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+}
